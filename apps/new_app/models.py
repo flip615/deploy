6e3b9a784cf79b321b16d8e0dc1f9271eff6ce10
GianFranco Macaraeg <gfmacaraeg@gmail.com>
2017-06-29 16:38:21 -0500

    initial commit

--- apps/new_app/models.py
+++ apps/new_app/models.py
@@ -1,71 +1,109 @@
 from __future__ import unicode_literals
-
 from django.db import models
 import re
+import bcrypt
+
 class UserManager(models.Manager):
 	def registration(self, form_data):
-		name =[]
-		alias =[]
+		firstname =[]
+		lastname =[]
 		email =[]
 		password = []
 		confirm =[]
-		if len(form_data['name'] ) < 2:
-			name.append('*Name cannot be less than 2 characters')
-		if len(form_data['alias'] )< 2:
-			alias.append('*Alias cannot be less than 2 characters')
+		birth =[]
+		boolerror = False
+		if len(form_data['firstname'] ) < 4:
+			firstname.append('*Name cannot be less than 4 characters')
+			boolerror = True
+		if len(form_data['lastname'] )< 4:
+			lastname.append('*Alias cannot be less than 4 characters')
+			boolerror = True
 		if not re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)' , form_data['email']):
 			email.append('*That is not a valid email format')
-		if len(form_data['password'] )== 0:
+			boolerror = True
+		duplicate = User.objects.filter(email = form_data['email']).first()
+		if duplicate:
+			email.append("That email has already been registered")
+			boolerror = True
+		if len(form_data['password'] ) == 0:
 			password.append('*Password is required')
-		if len(form_data['password'] )< 8:
+			boolerror = True
+		if len(form_data['password'] ) < 8:
 			password.append('*Password must be at least 8 characters')
+			boolerror = True
 		if form_data['password'] != form_data['confirm']:
 			confirm.append('*Password doesn\'t match')
+			boolerror = True
+		if len(form_data['birth'] ) == 0:
+			birth.append('*Birthdate is required')
+			boolerror = True	
 		errors = {
-		'name':name,
-		'alias':alias,
+		'firstname':firstname,
+		'lastname':lastname,
 		'email':email,
 		'password':password,
 		'confirm':confirm,
+		'boolerror':boolerror,
+		'birth':birth,
 		}
 		return errors
 
 	def validate(self,form_data):
 		email =[]
 		password = []
+		boolerror = False
 		if len(form_data['email']) == 0:
 			email.append('*Email cannot be blank')
+			boolerror = True
 		if len(form_data['password']) == 0:
 			password.append('*Password cannot be blank')
+			boolerror = True
+		varlogin = User.objects.filter(email=form_data['email']).first()
+		if varlogin:
+			userpassword = str(form_data['password'])
+			db_password = str(varlogin.password)
+			hashed_pw = bcrypt.hashpw(userpassword, db_password)
+			if hashed_pw == db_password:
+				boolerror = False
+			else:
+				password.append("*Password doesn't match our records")
+				boolerror = True
+		else:
+			email.append("*Email address does not match our records")
+			boolerror = True	
 		errors = {
 		'loginemail':email,
 		'loginpassword':password,
+		'boolerror':boolerror,
+		'user': varlogin
 		}
 		return errors
 
+	def create_user(self,form_data):
+		password = str(form_data['password'])
+		hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+		varuser = User.objects.create(firstname = form_data['firstname'],
+						 alias = form_data['lastname'], 
+						 email = form_data['email'], 
+						 birthdate = form_data['birth'], 
+						 password = hashed_pw)
+		return varuser
+
 class  User(models.Model):
-	name = models.CharField(max_length=255)
+	firstname = models.CharField(max_length=255)
 	alias = models.CharField(max_length=255)
 	email = models.EmailField(max_length=255)
 	password = models.CharField(max_length=255)
+	birthdate = models.DateField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 	objects = UserManager()
 
+class Poke(models.Model):
+	poked_user = models.ForeignKey(User,related_name ="pokeduser", on_delete=models.CASCADE)
+	poked_by = models.ForeignKey(User,related_name ="pokedby", on_delete=models.CASCADE)
 
-class Books(models.Model):
-	title = models.CharField(max_length=255)
-	author = models.CharField(max_length=255)
-	user = models.ForeignKey(User,related_name ="userbooks", on_delete=models.CASCADE)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
 
-class Reviews(models.Model):
-	user = models.ForeignKey(User,related_name ="userreviews", on_delete=models.CASCADE)
-	book = models.ForeignKey(Books,related_name ="bookreviews", on_delete=models.CASCADE)
-	content = models.TextField()
-	rating = models.CharField(max_length=5)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+
 
 
--- apps/new_app/views.py
+++ apps/new_app/views.py
@@ -1,155 +1,86 @@
 from django.shortcuts import render, redirect
 from .models import *
 import bcrypt
-def index(request):
+
+def main(request):
 	if 'user' in request.session:
-		return redirect('/books')
+		return redirect('/pokes')
 	if 'errors' not in request.session:
 		request.session['errors'] = {}
 	context = request.session['errors']
-
-	return render(request,'new_app/index.html',context)
+	request.session.pop('errors')
+	return render(request,'new_app/main.html',context)
 
 def registration(request):
 	if request.method =="POST":
-
 		form_data = request.POST
-		x = User.objects.registration(form_data)
-		request.session['errors'] = x
-		print "This is value of x", x
-		duplicate = User.objects.filter(email = form_data['email'])
-		if duplicate:
-			x['email'].append("That email has already been registered")
-		
-		if x['name'] == [] and x['password'] == [] and x['email'] == [] and x['confirm'] == []:
-			password = str(form_data['password'])
-			hashed_pw = bcrypt.hashpw(password,bcrypt.gensalt())
-			y = User.objects.create(name = form_data['name'], alias = form_data['alias'], email = form_data['email'], password = hashed_pw)
-			request.session['user'] = y.id
-			return redirect('/books')
+		varerror = User.objects.registration(form_data)
+		request.session['errors'] = varerror
+		if not varerror['boolerror']:
+			varuser = User.objects.create_user(form_data)
+			request.session['user'] = varuser.id
+			return redirect('/pokes')
 		else:
 			return redirect('/')
-	
+	return redirect('/')		
+
 def login(request):
 	if request.method == "POST":
 		form_data = request.POST
-		x=User.objects.validate(form_data)
-		if x['loginpassword'] == [] and x['loginemail'] == []:
-			varlogin = User.objects.filter(email=form_data['email']).first()
-			if varlogin:
-				password = str(form_data['password'])
-				db_password = str(varlogin.password)
-				hashed_pw = bcrypt.hashpw(password, db_password)
-				if hashed_pw == db_password:
-					request.session['user'] = varlogin.id
-					return redirect('/books')
-				else:
-					x['loginpassword'].append("Login or Password doesn't match our records")
-					request.session['errors'] = x
-					return redirect('/')
-			else:	
-				x['loginpassword'].append("Login or Password doesn't match our records")
-				request.session['errors'] = x
-				return redirect('/')		
+		validate = User.objects.validate(form_data)
+		if not validate['boolerror']:
+			request.session['user'] = validate['user'].id
+			return redirect('/pokes')
 		else:	
-			x['loginpassword'].append("Login or Password doesn't match our records")
-			request.session['errors'] = x
-			return redirect('/')
-	# else:	
-	# 	return redirect('/')	
+			errors = {
+				'loginemail':validate['loginemail'],
+				'loginpassword':validate['loginpassword'],
+				}
+			request.session['errors'] = errors
+			pass
+	return redirect('/')	
 
-def books(request):
+def pokes(request):
 	if 'user'	 in request.session:
 		user = User.objects.filter(id = request.session['user']).first()
-		# allbooks = Books.objects.raw("select * from new_app_books as b join new_app_reviews as r on b.id = r.book_id join new_app_user as u on r.user_id = u.id order by r.created_at desc limit 3")
-		allbooks = Books.objects.raw("select * from new_app_reviews as r left join new_app_books as b on r.book_id = b.id left join new_app_user u on r.user_id = u.id order by r.created_at desc limit 3")
-		allbooks2 = Books.objects.raw("select * from new_app_books as b join new_app_reviews as r on b.id = r.book_id join new_app_user as u on r.user_id = u.id")
+		otherusers = User.objects.exclude(id = user.id)
+		countpokes = Poke.objects.raw("""SELECT id, count(Distinct poked_by_id) as total
+							FROM new_app_poke p 
+							where poked_user_id = %s""", [user.id])
+	
+		allpokes = Poke.objects.raw("""SELECT user.id, user.firstname, user.alias, user.email, count(p.poked_by_id) as total
+							FROM new_app_user as user
+							left join new_app_poke as p 
+							on user.id = p.poked_user_id
+							group by user.id, user.firstname, user.alias, user.email """)
+
+		listpokes = Poke.objects.raw("""SELECT  p. id, p.poked_by_id,  user.firstname, count(p.poked_by_id) as total
+							FROM new_app_poke p
+							join new_app_user as user
+							on p.poked_by_id = user.id
+							where p.poked_user_id = %s
+							group by p.poked_by_id,  user.firstname
+							order by total desc""", [user.id])
+		
 		context = {
-		'first_name': user.name,
-		'booklist': allbooks,
-		'star': "12345",
-		'allbooks':allbooks2
+		'user': user,
+		'otherusers':otherusers,
+		'pokes': pokes,
+		'userpokes': countpokes,
+		'allpokes':allpokes,
+		'listpokes': listpokes
 		}
-	return render(request, 'new_app/books.html', context)
+	return render(request, 'new_app/pokes.html', context)
 
 def logout(request):
 	request.session.flush()
 	return redirect('/')
 
-def addbook(request):
-	x = Books.objects.raw("select id,  author from new_app_books group by author")
-	authors = []
-	for some in x:
-		authors.append(some.author)
-		print some.author
-	
-	context ={
-	'authorlist': authors,
-	}
-
-	return render(request,'new_app/add.html', context)
-
-def registerbook(request):
-	if request.method == "POST":
-		print "this is author1", request.POST['author1']
-		print "this is author2", request.POST['author2']
-		if request.POST['author1'] == "None":
-			author = request.POST['author2']
-		else:
-			author = request.POST['author1']	
-		x = Books.objects.create(title = request.POST['title'], author= author, user_id = request.session['user'])
-		y = Reviews.objects.create(user_id=x.user_id, book_id = x.id, rating = request.POST['rating'], content = request.POST['contentreview'])
-		request.session['book'] = x.id
-	return redirect('/books/999999')
-
-def showbooks(request,id):
-	if 'user'	 in request.session:
-		# book = Books.objects.raw("select * from new_app_books as b join new_app_reviews as r on b.id = r.book_id join new_app_user as u on r.user_id = u.id where b.id = %s",[id])
-		if id == "999999":
-			id = request.session['book']
-			request.session.pop('book')
-		book = Books.objects.filter(id=id).first()
-		user = User.objects.filter(id = request.session['user']).first()
-		print "This is the value of book", book
-		varbook = book.id
-		review = Reviews.objects.raw("select * from new_app_reviews as r left join new_app_books as b on r.book_id = b.id left join new_app_user u on r.user_id = u.id where b.id = %s", [varbook])
-		reviewed = []
-		for i in review:
-			reviewed.append(i.user_id)
-		context ={
-		'bookresult':book,
-		'review': review,
-		'star': "12345",
-		'reviewed':reviewed,
-		}
-	return render(request,'new_app/showbook.html', context)
-
-def registerbookshow(request,id):
-	if request.method == "POST":
-		request.session['book'] = id
-		user = User.objects.filter(id=request.session['user']).first()
-		varx = Reviews.objects.create(user_id = user.id, book_id = id, content = request.POST['contentreview'], rating = request.POST['rating'])
-		return redirect('/books/999999')
-
-def delete(request,id):
-	varuser = Reviews.objects.filter(id= id).first()
-	print varuser
-	if str(request.session['user']) == str(varuser.user_id):
-		varuser.delete()
-		request.session['book'] = varuser.book_id
-	return redirect('/books/999999')
-
-
-def users(request, id):
-	user = User.objects.filter(id=id).first()
-	x = Reviews.objects.raw("select b.id, b.title from new_app_reviews as r left join new_app_books as b on r.book_id = b.id left join new_app_user u on b.user_id = u.id where r.user_id = %s group by b.title", [id])
-	count = Reviews.objects.raw("select r.id, count(content) as total from new_app_reviews as r where r.user_id = %s", [id])
-	context = {
-	'user': user,
-	'booksreviewed': x,
-	'count': count
-	}
-	return render(request,'new_app/users.html',context)
+def addpoke(request,id):
+	if str(request.session['user']) != str(id):
+		varuser = Poke.objects.create(poked_user_id = id, poked_by_id = request.session['user'])
+		return redirect('/pokes')
+	return redirect('/pokes')
 
 
 
